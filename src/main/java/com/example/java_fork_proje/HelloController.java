126f685bcdd5c60defa8bd8338df12d5fe909ca5
mhmtgks <goksumehmetgs@gmail.com>
2022-05-21 10:48:57 +0300

    test

---
+++ src/main/java/com/example/java_fork_proje/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.java_fork_proje;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("mehmet first forkk!");
+    }
+}
